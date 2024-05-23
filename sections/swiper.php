668377f094027c5ffed71953ecf5b58644e3f4af
Dorian <deneuchateldorian205@gmail.com>
2024-05-23 21:51:46 +0200

    Chgm backgrnd img hover pj

--- sections/swiper.php
+++ sections/swiper.php
@@ -6,42 +6,42 @@
                     <!-- Slides -->
                     <<div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets//BBV2.png" alt="BBV2" data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets//BBV2.png" alt="BBV2" data-background="addons/imgs/projets//BBV2.png">
                         </a>
                     </div> 
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/After_Blast.png" alt="BBV2"  data-background="addons/imgs/LMJ_desc.jpg"> 
+                            <img class="thumbTile__image" src="addons/imgs/projets/After_Blast.png" alt="BBV2"  data-background="addons/imgs/projets/After_Blast.png"> 
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/Eventura.png" alt="Eventura"  data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets/Eventura.png" alt="Eventura"  data-background="addons/imgs/projets/Eventura.png">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/FJS.png" alt="FJS"  data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets/FJS.png" alt="FJS"  data-background="addons/imgs/projets/FJS.png">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/LMJ.png" alt="LMJ" data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets/LMJ.png" alt="LMJ" data-background="addons/imgs/projets/LMJ.png">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/RespireStat.png" alt="RespireStat"  data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets/RespireStat.png" alt="RespireStat"  data-background="addons/imgs/projets/RespireStat.png">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/ToolBox.png" alt="ToolBox"  data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets/ToolBox.png" alt="ToolBox"  data-background="addons/imgs/projets/ToolBox.png">
                         </a>
                     </div>
                     <div class="swiper-slide">
                         <a class="thumbTile" href="#">
-                            <img class="thumbTile__image" src="addons/imgs/projets/VR.png" alt="Projet VR"  data-background="addons/imgs/LMJ_desc.jpg">
+                            <img class="thumbTile__image" src="addons/imgs/projets/VR.png" alt="Projet VR"  data-background="addons/imgs/projets/VR.png">
                         </a>
                     </div>
                 </div>
