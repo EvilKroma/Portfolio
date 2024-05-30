2abf4fa75634b6c4308417ef168b8384e7737866
EvilKroma <deneuchateldorian205@gmail.com>
2024-05-30 22:52:54 +0200

    Revert "Btn scroll top"
    This reverts commit 9d8106dac35d9331adbfc4a35c2b8ab6fc100089.

--- pages_projets/Eventura.php
+++ pages_projets/Eventura.php
@@ -1,6 +1,6 @@
 <?php require "../header.php" ?>
 <body>
-    <div id="project-container" class="project-container">
+    <div class="project-container">
         <div class="back-link">
             <a href="../index.php">Retour</a>
         </div>
@@ -10,17 +10,13 @@
             <p>Website for Caroline Seiffert — a visual artist whose skill-on photographs are still life compositions of found everyday objects and all their surroundings. They arise from a natural flow of intuition and an open-hearted view.</p>
         </div>
         <div class="gallery">
-            <img src="../addons/imgs/projets/Eventura/G1.png" alt="Project Image 1">
-            <img src="../addons/imgs/projets/Eventura/G2.png" alt="Project Image 2">
-            <img src="../addons/imgs/projets/Eventura/G3.png" alt="Project Image 3">
-            <img src="../addons/imgs/projets/Eventura/G1.png" alt="Project Image 4">
-            <img src="../addons/imgs/projets/Eventura/G2.png" alt="Project Image 5">
-            <img src="../addons/imgs/projets/Eventura/G3.png" alt="Project Image 6">
+            <img src="image1.jpg" alt="Project Image 1">
+            <img src="image2.jpg" alt="Project Image 2">
+            <img src="image3.jpg" alt="Project Image 3">
+            <img src="image4.jpg" alt="Project Image 4">
+            <img src="image5.jpg" alt="Project Image 5">
+            <img src="image6.jpg" alt="Project Image 6">
         </div>
-        <a href="#project-container" class="scroll-to-top">
-            <ion-icon name="arrow-up-outline"></ion-icon>
-        </a>
-
     </div>
 </body>
 </html>
