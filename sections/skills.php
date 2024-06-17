005ead44dfa78cf3d1178a5025427e9cc97c3747
DENEUCHATEL-Dorian <deneuchateldorian205@gmail.com>
2024-06-17 14:38:16 +0200

    update skills

--- sections/skills.php
+++ sections/skills.php
@@ -3,21 +3,27 @@
 <section class="skills">
     <div class="skill">
         <img src="addons/imgs/skills/HTML.png" alt="HTML">
+        <h3 class="skill_name">HTML</h3>
     </div>
     <div class="skill">
         <img src="addons/imgs/skills/CSS.png" alt="CSS">
+        <h3 class="skill_name">CSS</h3>
     </div>
     <div class="skill">
         <img src="addons/imgs/skills/PHP.png" alt="PHP">
+        <h3 class="skill_name">PHP</h3>
     </div>
     <div class="skill">
         <img src="addons/imgs/skills/JAVA.png" alt="Java">
+        <h3 class="skill_name">Java</h3>
     </div>
-    <div class="skill">
-        <img src="addons/imgs/skills/JS.png" alt="JavaScript">
+    <div class="skill" >
+        <img id="JS"src="addons/imgs/skills/JS.png" alt="JavaScript">
+        <h3 class="skill_name">JavaScript</h3>
     </div>
     <div class="skill">
         <img src="addons/imgs/skills/SQL.png" alt="SQL">
+        <h3 class="skill_name">SQL</h3>
     </div>
 </section>
 
