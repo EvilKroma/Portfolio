f206485ee2ba27a22f2473ce10b17804feb0fce3
DENEUCHATEL-Dorian <deneuchateldorian205@gmail.com>
2024-06-18 18:02:13 +0200

    lien contacte

--- sections/contact.php
+++ sections/contact.php
@@ -1,8 +1,8 @@
 <section id="contact">
   <h1 class="section_titre">Contactez-moi</h1>
   <ul class="contact-list">
-      <li><a href="mailto:votre.email@example.com" class="mail"><img class="mail_svg" src="addons/imgs/icones/mail.svg" alt="">Envoyez-moi un mail</a></li>
-      <li><a href="https://github.com/votrecompte" class="github" target="_blank"><img class="github_svg" src="addons/imgs/icones/github.svg" alt="">Mon GitHub</a></li>
-      <li><a href="https://www.linkedin.com/in/votreprofil" class="linkedin" target="_blank"><img class="linkedin_svg" src="addons/imgs/icones/linkedin.svg" alt="">Mon LinkedIn</a></li>
+      <li><a href="mailto:deneuchateldorian@gmail.com" class="mail"><img class="mail_svg" src="addons/imgs/icones/mail.svg" alt="">Envoyez-moi un mail</a></li>
+      <li><a href="https://github.com/EvilKroma" class="github" target="_blank"><img class="github_svg" src="addons/imgs/icones/github.svg" alt="">Mon GitHub</a></li>
+      <li><a href="https://www.linkedin.com/in/dorian-deneuchatel-27928325a" class="linkedin" target="_blank"><img class="linkedin_svg" src="addons/imgs/icones/linkedin.svg" alt="">Mon LinkedIn</a></li>
   </ul>
 </section>
