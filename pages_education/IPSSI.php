0c6f121e21bb8514b54453b44f221fe27278912e
EvilKroma <deneuchateldorian205@gmail.com>
2024-06-24 15:33:03 +0200

    Page IPSSI update

--- pages_education/IPSSI.php
+++ pages_education/IPSSI.php
@@ -2,19 +2,42 @@
 <body>
     <div id="project-container" class="project-container eventura"> 
     <div class="project-header" style="background: url('../addons/imgs/education/ipssi.jpg') no-repeat center center/cover;">
-            <div class="back-link">
-                <a href="../index.php">Retour</a>
-            </div>
-            <p>Octobre 2023 à Juin 2025</p>
-            <h1>Ecole IPSSI MLV</h1>
-            <p>BTS SIO option SLAM</p>
+        <div class="back-link">
+            <a href="../index.php">Retour</a>
         </div>
-        <div class="gallery">
-            <!-- <img src="../addons/imgs/projets/Eventura/G1.png" alt="Project Image 1"> -->
-             <p class="page_desc">Le BTS SIO option SLAM est une formation de deux ans centrée sur les technologies informatiques appliquées aux besoins des organisations. Les étudiants y acquièrent des compétences avancées en développement logiciel, incluant la programmation, la conception d'applications, et la gestion de bases de données. Ils sont également formés à la gestion de projets informatiques, apprenant à planifier, organiser et superviser des initiatives technologiques au sein d'entreprises variées.</p>
-             <hr>
-            <p class="page_desc"> À l'issue de leur formation, les diplômés du BTS SIO option SLAM sont prêts à intégrer le marché du travail en tant que développeurs d'applications, analystes programmeurs, ou à poursuivre leurs études dans des filières spécialisées en informatique. Cette formation polyvalente et pratique répond aux besoins croissants en expertise technique dans un monde numérique en évolution constante, offrant aux étudiants des opportunités professionnelles diversifiées et stimulantes.</p>
+        <p>Octobre 2023 à Juin 2025</p>
+        <h1>Ecole IPSSI MLV</h1>
+        <p>BTS SIO option SLAM</p>
+    </div>
+    <div class="gallery">
+    <hr>
+    <h1 id="SLAMvsSISR" > BTS SIO option SLAM</h1>
+    <hr>
+    <div class="content">
+        <p class="page_desc">Le BTS SIO option SLAM est une formation de deux ans centrée sur les technologies informatiques appliquées aux besoins des organisations. Les étudiants y acquièrent des compétences avancées en développement logiciel, incluant la programmation, la conception d'applications, et la gestion de bases de données. Ils sont également formés à la gestion de projets informatiques, apprenant à planifier, organiser et superviser des initiatives technologiques au sein d'entreprises variées.</p>
+        <div class="image-container">
+            <img src="../addons/imgs/education/ipssi_2.jpg" alt="Project Image 1">
         </div>
+    </div>
+    <hr>
+    <h1 id="SLAMvsSISR" >SLAM ou SISR ?</h1>
+    <hr>
+    <div class="content reverse">
+        <p class="page_desc">Le BTS SIO (Services Informatiques aux Organisations) propose deux options principales : 
+        <br>
+        <br>
+        L'option SLAM, qui se concentre sur le développement de logiciels et d’applications métiers, avec des compétences en programmation, conception d’applications, et gestion de bases de données. Les débouchés incluent développeur d’applications et analyste programmeur. 
+        <br>
+        <br>
+        L'option SISR, qui forme des spécialistes en administration des systèmes et réseaux, couvrant l'installation, la configuration, et la sécurité des infrastructures réseau. Les débouchés incluent administrateur réseau et technicien de support informatique.</p>
+        <div class="image-container">
+            <img src="../addons/imgs/education/SISR_vs_SLAM.jpg" alt="Project Image 2">
+        </div>
+    </div>
+    <hr>
+</div>
+        <hr>
+    </div>
         <a href="#project-container" class="scroll-to-top">
             <ion-icon name="arrow-up-outline"></ion-icon>
         </a>
