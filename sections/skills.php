1cd48bf4adfc48219eb1d33c10e8ad2d4bcf3c81
Dorian <150785986+EvilKroma@users.noreply.github.com>
2024-11-04 18:30:06 +0100

    Skills

--- sections/skills.php
+++ sections/skills.php
@@ -1,29 +1,30 @@
 <?php require "header.php" ?>
-<h3 id="skills" class="section_titre">Mes compétences</h3>
 <section id="skills" class="skills_container">
-    <div class="skill">
-        <img src="addons/imgs/skills/HTML.png" alt="HTML">
-        <h3 class="skill_name">HTML</h3>
-    </div>
-    <div class="skill">
-        <img src="addons/imgs/skills/CSS.png" alt="CSS">
-        <h3 class="skill_name">CSS</h3>
-    </div>
-    <div class="skill">
-        <img src="addons/imgs/skills/PHP.png" alt="PHP">
-        <h3 class="skill_name">PHP</h3>
-    </div>
-    <div class="skill">
-        <img src="addons/imgs/skills/JAVA.png" alt="Java">
-        <h3 class="skill_name">Java</h3>
-    </div>
-    <div class="skill" >
-        <img id="JS"src="addons/imgs/skills/JS.png" alt="JavaScript">
-        <h3 class="skill_name">JavaScript</h3>
-    </div>
-    <div class="skill">
-        <img src="addons/imgs/skills/SQL.png" alt="SQL">
-        <h3 class="skill_name">SQL</h3>
+    <h3 class="section_titre">Mes compétences</h3>
+    <div class="skills-grid">
+        <div class="skill-card">
+            <img src="addons/imgs/skills/HTML.png" alt="HTML">
+            <h3>HTML</h3>
+        </div>
+        <div class="skill-card">
+            <img src="addons/imgs/skills/CSS.png" alt="CSS">
+            <h3>CSS</h3>
+        </div>
+        <div class="skill-card">
+            <img src="addons/imgs/skills/PHP.png" alt="PHP">
+            <h3>PHP</h3>
+        </div>
+        <div class="skill-card">
+            <img src="addons/imgs/skills/JAVA.png" alt="Java">
+            <h3>Java</h3>
+        </div>
+        <div class="skill-card">
+            <img src="addons/imgs/skills/JS.png" alt="JAVASCRIPT">
+            <h3>JavaScript</h3>
+        </div>
+        <div class="skill-card">
+            <img src="addons/imgs/skills/SQL.png" alt="SQL">
+            <h3>SQL</h3>
+        </div>
     </div>
 </section>
-
